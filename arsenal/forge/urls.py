5aa7c6eceafd341eb979ca670c72143e3959694f
Arnoldas Kundrotas <arnokund@gmail.com>
2022-12-02 14:36:28 +0200

    + bs page, ArmorListView, bs view, templates new and added

--- arsenal/forge/urls.py
+++ arsenal/forge/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blacksmiths', views.blacksmiths, name='blacksmiths')
+    path('blacksmiths', views.blacksmiths, name='blacksmiths'),
+    path('blacksmith/<int:blacksmith_id', views.blacksmith, name='blacksmith'),
+    path('armors/', views.ArmorListView.as_view(), name='armors')
 ]
--- arsenal/forge/views.py
+++ arsenal/forge/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from . models import ArmorType, Blacksmith, Armor
 
 def index(request):
@@ -20,3 +21,20 @@ def blacksmiths(request):
     return render(
         request, 'forge/blacksmiths.html', 
         {'blacksmiths': Blacksmith.objects.all()})
+
+def blacksmith(request, blacksmith_id):
+    return render(
+        request, 'forge/blacksmith.html', 
+        {'blacksmiths': get_object_or_404(Blacksmith, id=blacksmith_id)}
+    )
+
+
+class ArmorListView(ListView):
+    model = Armor
+    template_name = 'forge/armor_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['armors_count'] = self.get_queryset().count
+        return context
+        
